85092246a9d0e38145dae1c5c1eae5346298d271
lottame <lotta.melcher@code.berlin>
2024-04-09 10:54:12 +0200

    Change intro1 to be an actual intro

--- exercises/00_intro/intro1.py
+++ exercises/00_intro/intro1.py
@@ -1,6 +1,30 @@
+# If you've finished an exercise, you can keep playing around with it!
+# As long as the 'I AM NOT DONE' comment is in the file, pylet will not move on
+# to the next exercise, even if it runs successfully.
+# If you have solved the exercise and are ready for the next, remove the
+# 'I AM NOT DONE' comment below
+
 ### I AM NOT DONE
 
-print("hello world!")
-
-def test_sth():
-    assert True == False
+print("Welcome to")
+print(
+    """
+ .----------------.  .----------------.  .----------------.  .----------------.  .----------------. 
+| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
+| |   ______     | || |  ____  ____  | || |   _____      | || |  _________   | || |  _________   | |
+| |  |_   __ \   | || | |_  _||_  _| | || |  |_   _|     | || | |_   ___  |  | || | |  _   _  |  | |
+| |    | |__) |  | || |   \ \  / /   | || |    | |       | || |   | |_  \_|  | || | |_/ | | \_|  | |
+| |    |  ___/   | || |    \ \/ /    | || |    | |   _   | || |   |  _|  _   | || |     | |      | |
+| |   _| |_      | || |    _|  |_    | || |   _| |__/ |  | || |  _| |___/ |  | || |    _| |_     | |
+| |  |_____|     | || |   |______|   | || |  |________|  | || | |_________|  | || |   |_____|    | |
+| |              | || |              | || |              | || |              | || |              | |
+| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
+ '----------------'  '----------------'  '----------------'  '----------------'  '----------------' 
+"""
+)
+print(
+    """
+      This exercise is already compiling successfully. The following exercises will contain
+      errors for you to fix. To complete pylet, you will solve the exercises. Have fun!!
+      """
+)
