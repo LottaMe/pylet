e7bbd346606a4ff301ac3d77f0ca39059eb3d0a8
lottame <lotta.melcher@code.berlin>
2024-04-12 08:48:29 +0200

    Add basic_types1

---
+++ exercises/02_basic_types/basic_types1.py
@@ -0,0 +1,18 @@
+# basic_types1.py
+
+# Below, we tried to assign three strings to three variables, but the code won't run.
+# Can you fix it?
+
+
+### I AM NOT DONE
+
+my_string = "I am a string
+
+my_second_string = 'I'm also a string'
+
+my_third_string = "I said, "I am a string""
+
+
+print(my_string)
+print(my_second_string)
+print(my_third_string)
