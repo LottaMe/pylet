8458b92b9aa928971a3de1fc56045bf6274111c4
lottame <lotta.melcher@code.berlin>
2024-04-12 15:13:49 +0200

    Add first function exercise

---
+++ exercises/03_functions/functions1.py
@@ -0,0 +1,16 @@
+# functions1.py
+
+
+# Below we have defined a function, that should .
+# Can you finish it, so that it prints "hello" to the console?
+
+
+### I AM NOT DONE
+
+
+def say_hello():
+
+
+say_hello()
+say_hello()
+say_hello()
