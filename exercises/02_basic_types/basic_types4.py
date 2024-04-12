024b0510c28e972c9f6f5ac4f46b6f57e2de8f9a
lottame <lotta.melcher@code.berlin>
2024-04-12 10:04:03 +0200

    Add basic_types5

---
+++ exercises/02_basic_types/basic_types4.py
@@ -0,0 +1,25 @@
+# basic_types4.py
+
+
+
+# float(), int()
+# round()
+# %, //
+
+
+### I AM NOT DONE
+
+# use float() or int(), so that these assertions works
+int_5 = 5.0
+float_5 = 5
+
+assert isinstance(int_5, int)      
+assert isinstance(float_5, float)
+
+# use round() to make this assertion pass.
+assert 6.67 == 20/3
+
+# Fix the assertions, by replacing the None with the correct result.
+# Ask yourself: What do these operators do? How could they be useful?
+assert 3//2 == None
+assert 3%2 == None
