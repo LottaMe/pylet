c2e64f61bb17ed156974fb64fce2c507f12d51c5
lottame <lotta.melcher@code.berlin>
2024-04-12 15:25:35 +0200

    Add functions3

---
+++ exercises/03_functions/functions2.py
@@ -0,0 +1,23 @@
+# functions2.py
+
+
+# You now know how to create a function.
+# We now want to create a function that turns hours into minutes.
+# The function should take an int (hours) and return another int (minutes).
+# Please finish the function.
+# Note, the type declarations are optional.
+
+
+### I AM NOT DONE
+
+
+def hours_to_minutes(hours: int) -> int:
+    return
+
+# Don't modify code below
+
+def test_hours_to_minutes():
+    assert hours_to_minutes(1) == 60
+    assert hours_to_minutes(5) == 300
+    assert hours_to_minutes(5) == 360
+
