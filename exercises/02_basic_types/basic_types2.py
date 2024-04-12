3d76ec645c8e2d166640c425a70c112fd2a3b071
lottame <lotta.melcher@code.berlin>
2024-04-12 09:02:39 +0200

    Add basic_types2

---
+++ exercises/02_basic_types/basic_types2.py
@@ -0,0 +1,20 @@
+# basic_types1.py
+
+# We are given a time in hours and want to know what that time is in seconds.
+# Can you finish the code for us?
+# Let python do the math for you.
+
+
+### I AM NOT DONE
+
+
+time_in_hours = 33
+
+time_in_seconds = time_in_hours
+
+
+# Don't modify code below
+# You can take a look at it, if you want to get a preview of if statements.
+
+if(time_in_seconds != 1980):
+    raise ValueError(f"{time_in_seconds} is not {time_in_hours} hours in seconds.")
