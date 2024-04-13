1f9b56db2e4cbad22c60b9badba9ff212922b70f
lottame <lotta.melcher@code.berlin>
2024-04-13 12:07:11 +0200

    Add exercise conditions2

---
+++ exercises/04_conditions/conditions2.py
@@ -0,0 +1,19 @@
+# conditions2.py
+
+
+# We are printing a custom message, depending on what day of the week we have.
+# Can you finish the code?
+
+
+### I AM NOT DONE
+
+weekday = 
+
+if weekday == "Monday":
+    print("Oh no. The weekend is over")
+elif weekday == "Tuesday":
+    print("Tuesday, Bluesday")
+elif weekday == "Wednesday":
+    
+else:
+    print("That's not one of the days of the week :O")
