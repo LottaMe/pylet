ee3cdd0533f5d88e2a99e317b7e5813bf958f32a
lottame <lotta.melcher@code.berlin>
2024-04-29 12:46:23 +0200

    Add while loops 1

---
+++ exercises/05_while_loops/while_loops1.py
@@ -0,0 +1,21 @@
+# while_loops1.py
+
+
+# We've written a countdown for new years for you.
+# Try it out.
+# What happens if you remove line 19?
+
+
+
+import time
+
+counter = 
+
+while counter > 0:
+  print(counter)
+
+  time.sleep(1)
+  
+  counter = counter - 1
+
+print("Happy New Year!!")
