9ae98cbbfb1791d9bbd257fd123602026c6cd094
lottame <lotta.melcher@code.berlin>
2024-04-29 14:12:13 +0200

    Add exercise strings2

---
+++ exercises/06_strings/strings2.py
@@ -0,0 +1,22 @@
+# strings2.py
+
+
+# We want to figure out how three string methods work:
+# capitalize, upper and title
+# Change the "i am a string" strings on the right side of each expressions
+# so that the assert returns true.
+
+### I AM NOT DONE
+
+string = "i am a string"
+
+assert string.capitalize() == "i am a string"
+assert string.upper() == "i am a string"
+assert string.title() == "i am a string"
+
+
+# Don't modify code below
+
+print("capitalize():", string.capitalize())
+print("upper():", string.upper())
+print("title():", string.title())
