d4014e2666ae775f310b7927cfd147e920cf74ed
lottame <lotta.melcher@code.berlin>
2024-04-29 14:06:21 +0200

    Add exercise strings1

---
+++ exercises/06_strings/strings1.py
@@ -0,0 +1,60 @@
+# strings1.py
+
+
+# 
+
+### I AM NOT DONE
+
+def generate_lyrics(name: str):
+    return """
+Forgive me {name}
+My lost fearless leader
+In closets like cedar
+Preserved from when we were just kids
+Is it something I did
+The goddess of timing
+Once found us beguiling
+She said she was trying
+{name} was she lying
+My ribs get the feeling she did
+And I didn't want to come down
+I thought it was just goodbye for now
+"""
+
+print(generate_lyrics("Peter"))
+
+
+# Don't modify code below
+
+def test_generate_lyrics():
+    lyrics_peter = """
+Forgive me Peter
+My lost fearless leader
+In closets like cedar
+Preserved from when we were just kids
+Is it something I did
+The goddess of timing
+Once found us beguiling
+She said she was trying
+Peter was she lying
+My ribs get the feeling she did
+And I didn't want to come down
+I thought it was just goodbye for now
+"""
+    assert generate_lyrics("Peter") == lyrics_peter
+    lyrics_wendy = """
+Forgive me Wendy
+My lost fearless leader
+In closets like cedar
+Preserved from when we were just kids
+Is it something I did
+The goddess of timing
+Once found us beguiling
+She said she was trying
+Wendy was she lying
+My ribs get the feeling she did
+And I didn't want to come down
+I thought it was just goodbye for now
+"""
+    assert generate_lyrics("Wendy") == lyrics_wendy
+
