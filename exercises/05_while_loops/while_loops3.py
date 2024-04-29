bef40c6e669713af71d1ae90695a6a2a3c67d2f4
lottame <lotta.melcher@code.berlin>
2024-04-29 13:22:43 +0200

    Add while 3 exercise

---
+++ exercises/05_while_loops/while_loops3.py
@@ -0,0 +1,23 @@
+# while_loops2.py
+
+
+# We're trying to count till a number of your choosing,
+# but stop the loop after that number.
+# Can you do that WITHOUT changing the conditional of the while loop?
+# Hint: try out the break statement
+
+
+### I AM NOT DONE
+
+
+import time
+
+
+counter = 1
+
+while True:         # DON'T CHANGE THIS LINE
+    print(counter)
+
+    time.sleep(1)
+  
+    counter = counter + 1
