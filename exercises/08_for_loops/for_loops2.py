96829abb02aa6e02f29747897ccef3972ff985cb
lottame <lotta.melcher@code.berlin>
2024-04-30 11:35:30 +0200

    Add contents for for_loops2

---
+++ exercises/08_for_loops/for_loops2.py
@@ -0,0 +1,12 @@
+# for_loops2.py
+
+# Finish the for loop to print out each letter of string
+# Before finally the entire string is printed after the for loop.
+
+### I AM NOT DONE
+
+string = "VICTORY"
+
+for 
+
+print(string)
