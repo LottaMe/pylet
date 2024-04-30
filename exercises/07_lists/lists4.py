6f427d6cf6b44b8319c3789435ce8d0b9cd29db0
lottame <lotta.melcher@code.berlin>
2024-04-30 09:46:14 +0200

    Add content lists4

--- exercises/07_lists/lists4.py
+++ exercises/07_lists/lists4.py
@@ -1,10 +1,26 @@
 # lists4.py
 
-# 
+# Change the lists on the right side of the assert statements,
+# so that they pass.
+# Consider how the different methods to remove items from lists work.
 
 ### I AM NOT DONE
 
+remove_example = ["apple", "banana", "pear", "banana"]
+assert remove_example.remove("banana") == ["apple", "banana", "pear", "banana"]
 
+pop_example_1 = ["apple", "banana", "pear", "banana"]
+assert pop_example_1.pop() == ["apple", "banana", "pear", "banana"]
 
-# Don't modify code below, 
-# but you can take a look and try to understand it
+pop_example_2 = ["apple", "banana", "pear", "banana"]
+assert pop_example_2.pop(0) == ["apple", "banana", "pear", "banana"]
+
+clear_example = ["apple", "banana", "pear", "banana"]
+assert clear_example.clear() == ["apple", "banana", "pear", "banana"]
+
+del_example_1 = ["apple", "banana", "pear", "banana"]
+del del_example_1[0] 
+assert del_example_1 == ["apple", "banana", "pear", "banana"]
+
+del_example_2 = ["apple", "banana", "pear", "banana"]
+del del_example_2   # Note that this deletes the entire list. del_example_2 does not exist anymore
