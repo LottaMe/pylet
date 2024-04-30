e190cdbc4415c47a5f83f364f47fc36deb2dc950
lottame <lotta.melcher@code.berlin>
2024-04-30 10:49:58 +0200

    Add lists6 content

--- exercises/07_lists/lists6.py
+++ exercises/07_lists/lists6.py
@@ -1,10 +1,21 @@
 # lists6.py
 
-# 
+# We have tried to create a tuple of artists, but needed
+# to remove and add to it afterwards.
+# Can you figure out why the code doesn't work?
+# Should we use a list instead?
 
 ### I AM NOT DONE
 
+# trying to add or remove to tuple
+artists = (
+    "Taylor Swift",
+    "Billie Eilish",
+    "Olivia Rodrigo"
+)
 
+icon = artists.pop(0)
+artists.append("James Blunt")
 
-# Don't modify code below, 
-# but you can take a look and try to understand it
+print(icon)
+print(artists)
