d58ce6040536c9f98c30d3f26caad4ce071999b6
lottame <lotta.melcher@code.berlin>
2024-05-09 11:28:27 +0200

    Change error_handling3 to not have extra function

--- exercises/11_error_handling/error_handling3.py
+++ exercises/11_error_handling/error_handling3.py
@@ -1,7 +1,8 @@
 # error_handling3.py
 
-# We have a function maybe_raise_error that takes a boolean and if it is
-# True raises an Error. Finish the handle_error function, so it returns a list outputs, 
+# Our function handle_error tries to execute code
+# that might throw an error.
+# Finish the handle_error function, so it returns a list outputs, 
 # that either has a message "error", if maybe_raise_error threw an error, or the message
 # "no error", if maybe_raise_error threw no error.
 # in either case, it should add the message "bye bye :) and return the outputs list"
@@ -9,14 +10,15 @@
 ### I AM NOT DONE
 
 # don't change this function
-def maybe_raise_error(error: bool):
-    if error:
-        raise ValueError()
+
     
 def handle_error(error: bool):
     outputs = []
     try:
-        maybe_raise_error(error)  # don't change this line
+        # don't change this code block
+        if error:
+            raise ValueError()
+    # your code here
     
 
 # Don't modify code below
