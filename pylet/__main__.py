45e40f81230b1850c33f5911c37ac9e25f2125cd
lottame <lotta.melcher@code.berlin>
2024-05-21 11:14:01 +0200

    Add argparse to pylet

--- pylet/__main__.py
+++ pylet/__main__.py
@@ -1,8 +1,19 @@
+import argparse
 from interface import Interface
 from runner import Runner
 
 if __name__ == "__main__":
     interface = Interface()
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-c", "--command", required=False, type=str)
+    args = parser.parse_args()
+
+    if args.command:
+        print(args.command)
+        exit(0)
+
     runner = Runner(exercise_info_path="exercise_info.yaml", interface=interface)
 
     runner.run()
