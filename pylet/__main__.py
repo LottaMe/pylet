4c8eeb62f4d8467d47a859b301552600e59c2af6
lottame <lotta.melcher@code.berlin>
2024-05-21 14:27:22 +0200

    Refactor some

--- pylet/__main__.py
+++ pylet/__main__.py
@@ -2,24 +2,21 @@ import argparse
 from interface import Interface
 from runner import Runner
 
-def watch(args):
-    interface = Interface()
-    runner = Runner(exercise_info_path="exercise_info.yaml", interface=interface)
-
-    runner.run()
-
-def summary(args):
+def summary():
     print("summary not implemented yet")
 
-parser = argparse.ArgumentParser()
-subparsers = parser.add_subparsers()
-
-watch_parser = subparsers.add_parser("watch")
-watch_parser.set_defaults(func=watch)
-
-summary_parser = subparsers.add_parser("summary")
-summary_parser.set_defaults(func=summary)
-
 if __name__ == "__main__":
+    interface = Interface()
+    runner = Runner(exercise_info_path="exercise_info.yaml", interface=interface)
+    
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+    
+    watch_parser = subparsers.add_parser("watch")
+    watch_parser.set_defaults(func=runner.run)
+
+    summary_parser = subparsers.add_parser("summary")
+    summary_parser.set_defaults(func=summary)
+
     args = parser.parse_args()
-    args.func(args)  # call the default function
+    args.func()  # call the default function
